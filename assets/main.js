17e81806b2143a26fd5c92d7b9bd8be374b043f5
tridence <brianwanjalabarasa@gmail.com>
2022-06-22 16:28:32 +0300

    Cleared Assignment

--- assets/main.js
+++ assets/main.js
@@ -7,3 +7,24 @@ button5.addEventListener('click', () => {
   indicator.textContent = (parseInt(indicator.textContent) + 5).toString()
 })
 
+
+const button10 = document.querySelector("#plusTen")
+
+button10.addEventListener('click', () => {
+  disp.textContent = "added 10"
+  indicator.textContent = (parseInt(indicator.textContent) + 10).toString()
+})
+
+const button15 = document.querySelector("#plusFifteen")
+
+button15.addEventListener('click', () => {
+  disp.textContent = "added 15"
+  indicator.textContent = (parseInt(indicator.textContent) + 15).toString()
+})
+
+const button20 = document.querySelector("#plusTwenty")
+
+button20.addEventListener('click', () => {
+  disp.textContent = "added 20"
+  indicator.textContent = (parseInt(indicator.textContent) + 20).toString()
+})
